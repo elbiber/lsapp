578e7bdf5fa936cb0d720a51c87f68a16576efb1
Willy Weise <hellmuth.willy.weise@stud.fh-luebeck.de>
2019-12-13 16:35:55 +0100

    last push before downgrade

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+  <h1>Create Post</h1>
+  {!! Form::open(['action' => 'PostsController@store', 'method' =>'post']) !!}
+    <div class="form-group">
+      {{Form::label('title', 'Title')}}
+      {{Form::text('title', null, ['class' => 'form-control', 'placeholder' => 'Title'])}}
+    </div>
+    <div class="form-group">
+      {{Form::label('body', 'Body')}}
+      {{Form::textarea('body', null, ['class' => 'form-control', 'placeholder' => 'Body'])}}
+    </div>
+    {{Form::submit('Submit', ['class'=>'btn btn-primary'])}}
+  {!! Form::close() !!}
+@endsection
